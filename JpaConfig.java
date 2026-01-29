0803b9c8ed793e2e1729680214dc91bea40e9688
Akram164 <ranaakram43195@gmail.com>
2026-01-29 21:32:30 +0530

    Add files via upload

---
+++ JpaConfig.java
@@ -0,0 +1,63 @@
+package com.myorganisation.Config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.myorganisation.repository")
+public class JpaConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+
+        hikariDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/hms?createDatabaseIfNotExist=true&useSSL=false&serverTimezone=UTC");
+        hikariDataSource.setUsername("root");
+        hikariDataSource.setPassword("root");
+        hikariDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        return hikariDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.myorganisation.model");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setPersistenceProviderClass(org.hibernate.jpa.HibernatePersistenceProvider.class);
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        jpaProperties.setProperty("hibernate.show_sql", "true");
+
+        em.setJpaProperties(jpaProperties);
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
