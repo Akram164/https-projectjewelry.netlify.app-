a5399f61df77ed67801ddaaa6e1d828a2f65a3a9
Akram164 <ranaakram43195@gmail.com>
2026-02-19 23:11:44 +0530

    Add files via upload

--- ServerController.java
+++ ServerController.java
@@ -1,4 +1,17 @@
-package com.myorganisation.nexify.Controller;
+package com.myorganisation.apigateway.Controller;
 
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class ServerController {
+    @GetMapping
+    public ResponseEntity<String> apiGatewayStatus() {
+        return new ResponseEntity<>("API Gateway is live!", HttpStatusCode.valueOf(200));
+    }
 }
